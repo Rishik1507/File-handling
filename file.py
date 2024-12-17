11e4568443bce943bb9538afff920ee9a1ba1af4
Rishik1507 <microsoftrishik@gmail.com>
2024-12-17 21:29:37 +0530

    c

--- file.py
+++ file.py
@@ -1,3 +1,11 @@
 file=open("sample.txt")
 print(file.read())
 file.close()
+
+f=open("sample.txt","w")
+f.write("Hello, \n")
+f.write("I am Rishik")
+
+a=open("sample.txt","a")
+a.write("\n I am 14 years old..")
+
